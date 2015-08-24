7dcf8adb236f2605e6b564b588e8a6f879d93165
Kenneth Bogner <bognerkenny@gmail.com>
2015-08-24 14:41:01 -0400

    First Commit

---
+++ CEThreadingDemo/CarrierEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEThreadingDemo
+{
+    public class Quote
+    {
+        public string CarrierName { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CarrierEngine
+    {
+        public List<Quote> GetRates(params int[] subscriptionIDs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CEThreadingDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEThreadingDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarrierEngine ce = new CarrierEngine();
+            List<Quote> quotes = ce.GetRates();
+            foreach (Quote q in quotes)
+            {
+                Console.WriteLine("{0} at ${1}", q.CarrierName, q.Price);
+            }
+        }
+    }
+}
